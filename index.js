e057ff6d06814edd05ae05198eeb2995408f29ff
L. Cadet <85368055+Ludcad615@users.noreply.github.com>
2022-03-11 12:15:10 -0500

    commit tree fb11bb6cfd2bc5db25e6f20ee4d2933b98078931

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+    return a + b;
+  }
+
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+    }
+
+    function divide(a, b) {
+        return a / b;
+    }
+    
+    function add(a, b) {
+        return 1 + 2;
+      }
+    
+      function subtract(a, b) {
+        return b - a;
+      }
+      
+      function multiply(a, b) {
+        return 1 * 2;
+        }
+    
+        function divide(a, b) {
+            return a / b;
+        }
+        
+        
